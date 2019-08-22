e5efb25294b1c6c42faa19c3c629466524ba46e9
Jack Matasek <jackmatasek@Jacks-MacBook-Pro.local>
2019-08-21 22:21:46 -0500

    Actually wrote program

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
 int main(void){
-printf("success\n");
+
+	int n = 1;
+
+	while(n < 101){
+		if(n % 3 == 0){
+			if(n % 5 == 0){
+				printf("FizzBuzz\n");
+			}
+			if(n % 5 != 0){
+				printf("Fizz\n");
+			}
+		}
+		if(n % 5 == 0 && n % 3 != 0){
+			printf("Buzz\n");
+		}
+		if(n % 3 != 0 && n % 5 != 0){
+			printf("%d\n", n);
+		}
+			
+		n++;
+	}
 }
